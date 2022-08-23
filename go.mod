61846c6a3b7e7b59d993e0d1d69d755693f87b41
gurkan <gurkanyesilyurt07@gmail.com>
2022-08-23 17:41:55 +0300

    base

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/mudita33/mudita-log
+
+go 1.18
+
+require (
+	github.com/rs/zerolog v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ logging.go
@@ -0,0 +1,98 @@
+package logging
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"os"
+	"path"
+)
+
+// Configuration for logging
+type Config struct {
+	// Enable console logging
+	IsDebug bool
+
+	// Enable console logging
+	ConsoleLoggingEnabled bool
+
+	// EncodeLogsAsJson makes the log framework log JSON
+	EncodeLogsAsJson bool
+	// FileLoggingEnabled makes the framework log to a file
+	// the fields below can be skipped if this value is false!
+	FileLoggingEnabled bool
+	// Directory to log to to when filelogging is enabled
+	Directory string
+	// Filename is the name of the logfile which will be placed inside the directory
+	Filename string
+	// MaxSize the max size in MB of the logfile before it's rolled
+	MaxSize int
+	// MaxBackups the max number of rolled files to keep
+	MaxBackups int
+	// MaxAge the max age in days to keep a logfile
+	MaxAge int
+}
+
+type Logger struct {
+	*zerolog.Logger
+}
+
+type SeverityHook struct{}
+
+func (h SeverityHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
+	if level != zerolog.NoLevel {
+		e.Str("severity", level.String())
+	}
+}
+
+// Configure sets up the logging framework
+//
+// In production, the container logs will be collected and file logging should be disabled. However,
+// during development it's nicer to see logs as text and optionally write to a file when debugging
+// problems in the containerized pipeline
+//
+// The output log file will be located at /var/log/service-xyz/service-xyz.log and
+// will be rolled according to configuration set.
+func Configure(config Config) *Logger {
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if config.IsDebug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	if config.ConsoleLoggingEnabled {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Caller().Timestamp().Logger()
+	} else {
+		log.Logger = log.Hook(SeverityHook{}).With().Caller().Timestamp().Logger()
+	}
+
+	log.Logger.Info().
+		Bool("debugLogging", config.IsDebug).
+		Bool("fileLogging", config.FileLoggingEnabled).
+		Bool("jsonLogOutput", config.EncodeLogsAsJson).
+		Str("logDirectory", config.Directory).
+		Str("fileName", config.Filename).
+		Int("maxSizeMB", config.MaxSize).
+		Int("maxBackups", config.MaxBackups).
+		Int("maxAgeInDays", config.MaxAge).
+		Msg("logging configured")
+
+	return &Logger{
+		Logger: &log.Logger,
+	}
+}
+
+func newRollingFile(config Config) io.Writer {
+	if err := os.MkdirAll(config.Directory, 0744); err != nil {
+		log.Error().Err(err).Str("path", config.Directory).Msg("can't create log directory")
+		return nil
+	}
+
+	return &lumberjack.Logger{
+		Filename:   path.Join(config.Directory, config.Filename),
+		MaxBackups: config.MaxBackups, // files
+		MaxSize:    config.MaxSize,    // megabytes
+		MaxAge:     config.MaxAge,     // days
+		LocalTime:  true,
+	}
+}
